f3af53bd1d68834f986094f3956d8a230edbf230
lazyrabbit-onfish <fish@lazyrabbit.xyz>
2020-09-10 16:20:04 +0800

    添加sm加密测试

---
+++ src/test/java/xyz/lazyrabbit/encryption/sm3/SM3Test.java
@@ -0,0 +1,22 @@
+package xyz.lazyrabbit.encryption.sm3;
+
+import org.bouncycastle.util.encoders.Hex;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @Description:
+ * @Author: wucheng
+ * @CreateDate: 2020/2/16 16:42
+ */
+public class SM3Test {
+
+    @Test
+    public void testSm3Digest() throws Exception {
+        String input = "test input";
+        byte[] ret = SM3Helper.digest(input.getBytes("utf-8"));
+
+        Assert.assertNotNull("SM3 test digest failed!", ret);
+        System.out.println("sm3 digest result:" + Hex.toHexString(ret));
+    }
+}
